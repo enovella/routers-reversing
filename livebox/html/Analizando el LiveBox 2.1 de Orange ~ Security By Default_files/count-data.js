19ee7fde148d46a417ce76a2b8769e486c36b5c4
Eduardo Novella <ednolo@inf.upv.es>
2018-04-15 12:58:21 +0200

    livebox reversing doc & tools (spanish)

---
+++ livebox/html/Analizando el LiveBox 2.1 de Orange ~ Security By Default_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"y","comments":{"zero":"0 comentarios","multiple":"{num} comentarios","one":"1 comentario"}},"counts":[{"id":"http:\/\/www.securitybydefault.com\/2015\/06\/analizando-el-livebox-21-de-orange.html","comments":4}]});
+}
